f3886c3b079480df1c58d4872163acd44d7e4418
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-10 01:18:39 -0700

    Advanced project is doon

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora korpar, 2015-10-19.
+ *   followed by a new line to the standard error.
+ * Return: If succsied the program should return 1
+ */
+int main(void)
+{
+write(2, "and that piece art is useful\" - Dora kopar, 2015-10-19\n", 59);
+return (1);
+}
