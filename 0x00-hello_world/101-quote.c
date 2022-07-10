01dbb986b78cad7778ea12a0bae224ed666be89a
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-10 02:10:55 -0700

    now is ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - prints exactly "and taht piece of art is useful"
  * - Dora korpar, 2015-10-19",
- * followed by a new line to the standard error.
- * Return: If succsied the program should return 1
+ * followed by a new line, to the standard error.
+ * Return: 1 if success
  */
 int main(void)
 {
