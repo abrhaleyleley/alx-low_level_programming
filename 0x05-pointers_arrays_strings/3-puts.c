bef535d2bc6f3a8ce06cc8b46997935fdc29da7c
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-12 12:51:47 -0700

    this is put function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_puts -> a function that puts
+ *@str: a parameter to _puts function
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str)i
+	}
+		_putchar('\n');
+}
