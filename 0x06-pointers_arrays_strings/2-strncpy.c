71e89b7b5302430d4479e4f6a673524de1a3f639
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-14 14:04:14 -0700

    string assignment

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> this function is copies a string
+ * @dest: 1st parameter
+ * @src: 2nd parameter
+ * @n: 3rd prameter
+ * Return: will return string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	srtncpy(dest, src, n);
+	return (dest);
+}
