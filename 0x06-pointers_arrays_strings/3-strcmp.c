0e9f7f0316412b376ca765b5016aa8478fb43d49
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-14 19:48:25 -0700

    comparing characters

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp -> a function compares two strings
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
