f55a85da28f042b65003cd30df2f0927f84226cf
abrhaleyleley <abrhaleyb2@gmail.com>
2022-07-14 19:23:43 -0700

    copying strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <string.h>
-
 /**
- * _strncpy -> this function is copies a string
+ * _strncpy -> for copying purpose
  * @dest: 1st parameter
  * @src: 2nd parameter
- * @n: 3rd prameter
- * Return: will return string
+ * @n: 3rd parameter
+ * Return: string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	strncpy(dest, src. n);
 	return (dest);
 }
